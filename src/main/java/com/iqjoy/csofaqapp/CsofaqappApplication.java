a3f52e1a82cc4e09b56d7701d1fafc422033c062
Adetunji Ayoade <ayoadejoe@outlook.com>
2023-03-27 14:48:19 +0100

    After send question issues rectified

--- src/main/java/com/iqjoy/csofaqapp/CsofaqappApplication.java
+++ src/main/java/com/iqjoy/csofaqapp/CsofaqappApplication.java
@@ -2,7 +2,7 @@ package com.iqjoy.csofaqapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//Code developed by Ayoade Adetunji 27/03/2023
 @SpringBootApplication
 public class CsofaqappApplication {
 
