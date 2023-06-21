532b540af946421c8ecdb2c6795841ebc5d0ec37
ayoadejoe <ayoadejoe@outlook.com>
2023-06-21 18:10:04 +0100

    As of 21/06/2023

--- src/main/java/com/iqjoy/csofaq/controllers/FAQRequestEnd.java
+++ src/main/java/com/iqjoy/csofaq/controllers/FAQRequestEnd.java
@@ -28,6 +28,7 @@ public class FAQRequestEnd {
 
     @PostMapping("/existing_faq")
     public List<CSOFAQs> getAllChangeRequests(@RequestBody Map<String, String> request) {
+        System.out.println("getAllChangeRequests: "+request);
         String username = request.get("username");
         String password = request.get("password");
 
@@ -38,11 +39,13 @@ public class FAQRequestEnd {
 
     @GetMapping("/requests")
     public List<NewFAQRequest> getAllNewRequests() {
-            return newFAQRequestRepository.findAll();
+        System.out.println("getting ALL ");
+        return newFAQRequestRepository.findAll();
     }
 
     @PostMapping("/save_response")
     public List<CSOFAQs> saveResponse(@RequestBody Map<String, String> request) {
+        System.out.println("saving CSOs: "+request);
         String response = request.get("response");
         String question = request.get("question");
         String regulator = request.get("regulator");
@@ -60,5 +63,49 @@ public class FAQRequestEnd {
         csofaQsRepository.save(csofaQs);
 
         return csofaQsRepository.findAll();
+    }
+
+    @PostMapping("/delete_faq")
+    public List<CSOFAQs> deleteChangeRequests(@RequestBody Map<String, String> request) {
+        System.out.println("Deleted id: "+request);
+        String username = request.get("username");
+        String password = request.get("password");
+        int id = Integer.parseInt(request.get("id").trim());
+
+        if(username.equals("admin") && password.equals("password")){
+            csofaQsRepository.deleteById(id);
+            System.out.println("Deleted id: "+id);
+            return csofaQsRepository.findAll();
+        }else return new ArrayList<CSOFAQs>();
+    }
+
+    @PostMapping("/delete_faq_request")
+    public List<NewFAQRequest> deleteFAQRequests(@RequestBody Map<String, String> request) {
+        System.out.println("Deleted id: "+request);
+        String username = request.get("username");
+        String password = request.get("password");
+        Long id = Long.parseLong(request.get("id").trim());
+
+        if(username.equals("admin") && password.equals("password")){
+            newFAQRequestRepository.deleteById(id);
+            System.out.println("Deleted id: "+id);
+            return newFAQRequestRepository.findAll();
+        }else return new ArrayList<NewFAQRequest>();
+    }
+
+    @PostMapping("/mail_sent_approval")
+    public List<NewFAQRequest> mailFAQResponse(@RequestBody Map<String, String> request) {
+        System.out.println("mailFAQResponse approving email id: "+request);
+        String username = request.get("username");
+        String password = request.get("password");
+        Long id = Long.parseLong(request.get("id").trim());
+
+        if(username.equals("admin") && password.equals("password")){
+            NewFAQRequest newFAQRequest = newFAQRequestRepository.getReferenceById(id);
+            newFAQRequest.setApproved(true);
+            newFAQRequestRepository.save(newFAQRequest);
+            System.out.println("saved approval for: "+id);
+            return newFAQRequestRepository.findAll();
+        }else return new ArrayList<NewFAQRequest>();
     }
 }
